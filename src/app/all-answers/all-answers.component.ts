96fc4105c6890b931d7fb060c384f43463b3bac2
Andrey <space.for.work@gmail.com>
2019-02-20 10:51:20 +0300

    App stub.

---
+++ src/app/all-answers/all-answers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-answers',
+  templateUrl: './all-answers.component.html',
+  styleUrls: ['./all-answers.component.scss']
+})
+export class AllAnswersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SignComponent } from './sign/sign.component';
+import { MyAnswerComponent } from './my-answer/my-answer.component';
+import { AllAnswersComponent } from './all-answers/all-answers.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MainComponent, SignComponent, MyAnswerComponent, AllAnswersComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
